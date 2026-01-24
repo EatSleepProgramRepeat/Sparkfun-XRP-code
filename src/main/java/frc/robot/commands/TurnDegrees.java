c04987d081e3a62b2ba89ad6ad84ed06f322fa97
Alexander McLean <mcleanalexander37@gmail.com>
2026-01-24 14:41:46 -0500

    Initial commit

---
+++ src/main/java/frc/robot/commands/TurnDegrees.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TurnDegrees extends Command {
+  private final Drivetrain m_drive;
+  private final double m_degrees;
+  private final double m_speed;
+
+  /**
+   * Creates a new TurnDegrees. This command will turn your robot for a desired rotation (in
+   * degrees) and rotational speed.
+   *
+   * @param speed The speed which the robot will drive. Negative is in reverse.
+   * @param degrees Degrees to turn. Leverages encoders to compare distance.
+   * @param drive The drive subsystem on which this command will run
+   */
+  public TurnDegrees(double speed, double degrees, Drivetrain drive) {
+    m_degrees = degrees;
+    m_speed = speed;
+    m_drive = drive;
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // Set motors to stop, read encoder values for starting point
+    m_drive.arcadeDrive(0, 0);
+    m_drive.resetEncoders();
+    // Reset gyro so we measure heading change from zero
+    m_drive.resetGyro();
+    System.out.println("TurnDegrees.initialize: target=" + m_degrees + " speed=" + m_speed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.arcadeDrive(0, m_speed);
+    System.out.println("TurnDegrees.execute: speed=" + m_speed + " gyro=" + m_drive.getGyroAngleZ());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.arcadeDrive(0, 0);
+    System.out.println("TurnDegrees.end: interrupted=" + interrupted + " finalGyro=" + m_drive.getGyroAngleZ());
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // Use the onboard gyro to determine when we've reached the desired rotation.
+    double currentAngle = Math.abs(m_drive.getGyroAngleZ());
+    System.out.println(
+        "TurnDegrees.isFinished: target=" + m_degrees + " currentGyro=" + currentAngle + " leftIn="
+            + Math.abs(m_drive.getLeftDistanceInch()) + " rightIn=" + Math.abs(m_drive.getRightDistanceInch()));
+    return currentAngle >= Math.abs(m_degrees);
+  }
+
+  private double getAverageTurningDistance() {
+    double leftDistance = Math.abs(m_drive.getLeftDistanceInch());
+    double rightDistance = Math.abs(m_drive.getRightDistanceInch());
+    return (leftDistance + rightDistance) / 2.0;
+  }
+}
